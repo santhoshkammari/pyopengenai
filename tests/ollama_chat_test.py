348f7d5b1fb0928e2017da73b1c434d4458bc94d
ksanthoshNT <kammari.santhosh@newgensoft.com>
2024-09-16 22:58:49 +0530

    pygenai

--- tests/ollama_chat_test.py
+++ tests/ollama_chat_test.py
@@ -1,5 +1,5 @@
 
-from src.opengenai.langchain_ollama import CustomChatOllama
+from src.pygenai.langchain_ollama import CustomChatOllama
 
 llm = CustomChatOllama(model="qwen2:0.5b", base_url="http://192.168.162.49:8888")
 print(llm.invoke("who are you?").content)
