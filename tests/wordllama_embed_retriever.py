0d68eded0f35bf58c905b1d858f7cd48323c7e7a
santhoshkammari <santhoshkammari1999@gmail.com>
2024-09-26 02:38:09 +0530

    fixed searchretriever

--- tests/wordllama_embed_retriever.py
+++ tests/wordllama_embed_retriever.py
@@ -1,16 +1,12 @@
-from langchain_core.language_models import BaseChatModel
-from langchain_core.output_parsers import JsonOutputParser
-from pyopengenai import QueryRefiner, SearchQueryToNSubquery
-from pyopengenai.query_master import SearchRetriever
-
 from langchain_ollama import ChatOllama
-
+from pyopengenai.ai_searcher import AdvancedAISearcher
 
 llm = ChatOllama(model = "qwen2.5:1.5b-instruct",
                  temperature = 0,
                  num_predict=8_000)
-query  = "i want KQML multiagent query language explanation"
+query  = "i want KQML multiagent query language algorithm better to search in arxiv papers"
 
-
+ans = AdvancedAISearcher.search(llm, query)
+print(ans)
 
 
